1c7188f64cd4ea8bbbb0e005ce5abad10da761f2
kadek srd <kadeksuryard13@gmail.com>
2023-03-29 16:01:06 +0700

    add brand icon

---
+++ src/parts/iconText.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "element/Button";
+
+function iconText() {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      Life <span className="text-gray-900">cation</span>
+    </Button>
+  );
+}
+export default iconText;
