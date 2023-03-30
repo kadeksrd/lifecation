22a36bd026f2714a1a5e36404a8b54ee78702bbf
kadek srd <kadeksuryard13@gmail.com>
2023-03-30 21:06:49 +0700

    header fixed style

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -8,28 +8,28 @@ function Header(props) {
   };
 
   return (
-    <div className="spacing-sm">
+    <header className="spacing-sm">
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light">
           <BrandIcon />
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav ml-auto">
-              <li className={`nav-item${getNavLinkClass("/")}`}>
+              <li className={`nav-item ${getNavLinkClass("/")}`}>
                 <Button className="nav-link" type="link" href="/">
                   Home
                 </Button>
               </li>
-              <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+              <li className={`nav-item ${getNavLinkClass("/browse-by")}`}>
                 <Button className="nav-link" type="link" href="/browse-by">
                   Browse By
                 </Button>
               </li>
-              <li className={`nav-item${getNavLinkClass("/stories")}`}>
+              <li className={`nav-item ${getNavLinkClass("/stories")}`}>
                 <Button className="nav-link" type="link" href="/stories">
                   Stories
                 </Button>
               </li>
-              <li className={`nav-item${getNavLinkClass("/agents")}`}>
+              <li className={`nav-item ${getNavLinkClass("/agents")}`}>
                 <Button className="nav-link" type="link" href="/agents">
                   Agents
                 </Button>
@@ -38,7 +38,7 @@ function Header(props) {
           </div>
         </nav>
       </div>
-    </div>
+    </header>
   );
 }
 
