2e4806d9931de5466b1c7d2ed4a1b4656db65ac4
kadek srd <kadeksuryard13@gmail.com>
2023-04-10 22:24:16 +0700

    add testimony part 1

--- src/page/LandingPage.js
+++ src/page/LandingPage.js
@@ -4,6 +4,7 @@ import Hero from "parts/Hero.js";
 import landingPage from "json/landingPage.json";
 import MostPicked from "parts/MostPicked";
 import Category from "parts/Category";
+import Testimony from "parts/Testimony";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -22,6 +23,7 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Category data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
       </>
     );
   }
