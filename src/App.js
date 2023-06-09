3c09766a0228e122c385e50865bc404f7088892d
kadek srd <kadeksuryard13@gmail.com>
2023-06-10 00:03:51 +0700

    update form elements

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "page/LandingPage";
+import Example from "page/example";
 
 function App() {
   return (
