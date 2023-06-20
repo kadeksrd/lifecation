989ec471b8ea7cff6b4d19ea7ed99d73e2bfc012
kadek srd <kadeksuryard13@gmail.com>
2023-06-20 22:35:04 +0700

    detail-page (featured -image)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "page/LandingPage";
 import Example from "page/example";
+import DetailsPage from "page/DetailsPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/propeties/:id" component={DetailsPage}></Route>
+        <Route exact path="/example" component={Example}></Route>
       </Router>
     </div>
   );
--- src/page/example.js
+++ src/page/example.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import InputDate from "element/Form/inputDate/index";
+import InputNumber from "element/Form/inputNumber/index";
+import Breadcrumb from "element/Breadcrumb";
 
 export default class Example extends Component {
   state = {
@@ -9,6 +11,12 @@ export default class Example extends Component {
       key: "selection",
     },
   };
+
+  breadcrumbList = [
+    { pageTitle: "Home", pageHref: "" },
+    { pageTitle: "House Details", pageHref: "" },
+  ];
+
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
@@ -16,10 +24,13 @@ export default class Example extends Component {
     console.log(this.state);
     return (
       <div className="container">
+        <Breadcrumb data={this.breadcrumbList} />
+
         <div
           className="row align-items-center justify-content-center"
           style={{ height: "100vh" }}>
           <div className="col-auto">
+            <InputNumber min={0} max={30} />
             <InputDate
               max={30}
               onChange={this.handleChange}
